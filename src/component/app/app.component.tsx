99d03020f745ae77a43cfd864e908d623c8422ef
Kyryl Petkov <sardonyx.wt@gmail.com>
2020-09-11 16:41:40 +0300

    - init.

---
+++ src/component/app/app.component.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+export const App: React.FC = () => {
+    return (
+        <div className="test">
+            Hello! <br />
+            You can use javascript or typescript. <br />
+            For styles use: styled-components or scss / sass.
+        </div>
+    );
+};
